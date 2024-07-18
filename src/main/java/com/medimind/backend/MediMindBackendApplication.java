8809d011e8dbdeb40cac29f0a96c39bc45bc6838
동욱 김 <dukim@ateciot.com>
2024-07-18 10:37:15 +0900

    Initial Commit

---
+++ src/main/java/com/medimind/backend/MediMindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medimind.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediMindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediMindBackendApplication.class, args);
+	}
+
+}
