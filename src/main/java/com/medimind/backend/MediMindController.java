1221885a3d643f67096ecc35961dda50cc236d05
동욱 김 <dukim@ateciot.com>
2024-07-19 09:56:35 +0900

    ADD API base

--- src/main/java/com/medimind/backend/MediMindController.java
+++ src/main/java/com/medimind/backend/MediMindController.java
@@ -1,5 +1,35 @@
 package com.medimind.backend;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MediMindController {
 
+	@ResponseBody
+	@PostMapping(value="/getChatData")
+	public String getChatData(String id) {
+		String result = "";
+		
+		return result;
+	}
+	
+	@ResponseBody
+	@PostMapping(value="/sendChat")
+	public String sendChat(String id, String chat) {
+		String result = "";
+		
+		result = requestChatbot(chat);
+		
+		return result;
+	}
+	
+	private String requestChatbot(String chat) {
+		String result = "";
+		
+		result = "챗봇의 답변";
+		
+		return result;
+	}
 }
